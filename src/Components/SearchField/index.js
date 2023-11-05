27e6e02a3a6e54c324c99660d58f11ed341c7208
Akash V <aakashvshetty@gmail.com>
2023-11-05 22:30:41 +0530

    design updates

--- src/Components/SearchField/index.js
+++ src/Components/SearchField/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchField = () => {
   return (
     <div>
-        <input/>
+        <input className='searchfield' placeholder='Search a album of your choice'/>
     </div>
   )
 }
