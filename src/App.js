00751cc4f4e55c677bdecc7b67fed9121ee2601e
Akash V <aakashvshetty@gmail.com>
2023-11-07 20:56:06 +0530

    hero qtify takehome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import HeroSection from './Components/HeroSection';
 import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
---
+++ src/Components/HeroSection/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+const HeroSection = () => {
+  return (
+    <div className='herosection'>
+        <div className='text'>
+       <span>100 Thousand Songs, ad-free</span> 
+       <span>Over thousands podcast episodes</span>
+        </div>
+        <div className='image'>
+            <img src='./vibrating-headphone 1.png' alt='Enjoy the feel of music'/>
+        </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
